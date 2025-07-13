b3cdb5138cf715ffefd055ca2696fd64eaf4de46
mariyasneha <mariyasneha104@gmail.com>
2025-07-13 22:21:38 +0530

    second commit

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -5,7 +5,7 @@ const AddProduct = () => {
     return (
         <div>
             <div className="container">
-                <ProductNavBar/>
+                <ProductNavBar />
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                         <div className="row g-3">
